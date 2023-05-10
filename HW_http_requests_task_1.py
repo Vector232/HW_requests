cc5f0f9405b0903fa6ed96d4cc3124f800e60640
Vlad Troyan <030705vt@mail.ru>
2023-05-10 15:34:01 +0300

    tasks completed

--- HW_http_requests_task_1.py
+++ HW_http_requests_task_1.py
@@ -1,1 +1,17 @@
-1
+"""Нужно определить кто самый умный(intelligence) из трех супергероев- Hulk, Captain America, Thanos."""
+import requests
+
+
+def SortForPowerstats(powerstats_name: str, hero_name_list: list):
+
+    url = 'https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api'
+    hero_list = requests.get(f'{url}/all.json').json()
+
+    sorted_hero_list = sorted([hero for hero in hero_list if hero['name'] in hero_name_list], key=lambda x: x['powerstats'][powerstats_name], reverse=True)
+
+    HeroTopList = {powerstats_name: {hero['name']: hero['powerstats'][powerstats_name] for hero in sorted_hero_list}}
+
+    return HeroTopList
+
+
+print(SortForPowerstats('intelligence', ['Hulk', 'Captain America', 'Thanos']))
--- HW_http_requests_task_3.py
+++ HW_http_requests_task_3.py
@@ -1,1 +1,21 @@
-1
+'''Нужно написать программу, которая выводит все вопросы за последние два дня и содержит тэг "Python".'''
+
+import requests
+import datetime
+
+def allQuestionsWithЕheTag(tag):
+    now = int(datetime.datetime.utcnow().timestamp())
+
+    url = 'https://api.stackexchange.com/2.3'
+    res = requests.get(f'{url}/questions?order=desc&tagged=Python&site=stackoverflow').json()
+
+    sorted_res_list = sorted([question for question in res['items'] if question['creation_date'] >= now - 172800], key=lambda x: x['creation_date'], reverse=True)
+
+    return sorted_res_list
+
+
+ans = allQuestionsWithЕheTag('Python')
+
+for qestion in ans:
+    print(datetime.datetime.fromtimestamp(qestion['creation_date']), qestion['tags'])
+    
